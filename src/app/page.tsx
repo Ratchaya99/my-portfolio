d1e3fc56ccaa3249115cd6ce4e91171c369d8290
Ratchaya S <ratchayas@MacBook-Air-khxng-Ratchaya.local>
2025-03-23 18:25:47 +0700

    feat(highlight-project): add highlighted projects section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,25 +2,35 @@
 import React, { useEffect, useState } from "react";
 import IntroduceView from "../components/introduce/introduce-view";
 import { useTheme } from "@/lib/provider/app-context-provider";
+import HighlightedProjectCard from "@/components/highlighted-projects/highlighted-project-card";
+import { Project } from "@/lib/model/project.model";
+import { fetchHighlightedProjects } from "@/lib/modules/projects/project.action";
 
 export default function Home() {
   const { themeColor } = useTheme();
   const [activeSection, setActiveSection] = useState<string>("");
+  const [projects, setProjects] = useState<Project[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    const options = {
-      root: null,
-      rootMargin: "0px",
-      threshold: 0.5,
+    const fetchData = async () => {
+      const highlighProjects = await fetchHighlightedProjects();
+      setProjects(highlighProjects);
+      setLoading(false);
     };
 
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          setActiveSection(entry.target.id);
-        }
-      });
-    }, options);
+    fetchData();
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setActiveSection(entry.target.id);
+          }
+        });
+      },
+      { root: null, rootMargin: "0px", threshold: 0.5 }
+    );
 
     const sections = document.querySelectorAll("section");
     sections.forEach((section) => observer.observe(section));
@@ -33,6 +43,7 @@ export default function Home() {
   return (
     <div className="w-full flex flex-col items-center bg-gray-100 text-gray-900">
       <div className="max-w-screen-lg w-full px-5">
+        {/* About Section */}
         <section
           id="about"
           className={`transition-all duration-500 ease-in-out ${
@@ -40,6 +51,30 @@ export default function Home() {
           }`}
         >
           <IntroduceView themeColor={themeColor} />
+        </section>
+
+        {/* Highlighted Projects Section */}
+        <section
+          id="hilighted-projects"
+          className={`transition-all duration-500 ease-in-out ${
+            activeSection === "hilighted-projects" ? "active" : ""
+          } flex flex-col gap-4 justify-center items-center min-h-screen h-auto py-5`}
+        >
+          <h2 className="text-2xl font-bold">Highlighted Projects</h2>
+
+          {loading ? (
+            <p className="text-center text-gray-500">Loading projects...</p>
+          ) : (
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+              {projects.map((project) => (
+                <HighlightedProjectCard
+                  key={`hilighted-projects-${project.id}`}
+                  item={project}
+                  themeColor={themeColor}
+                />
+              ))}
+            </div>
+          )}
         </section>
       </div>
     </div>
---
+++ src/lib/mocks/highlighted-projects.ts
@@ -0,0 +1,18 @@
+import { Project } from "../model/project.model";
+
+export const mockHighlightedProjects: Project[] = [
+  {
+    id: 1,
+    name: "Village Fund",
+    excerpt:
+      "Village Fund is an application designed to manage village fund data. It is a government initiative aimed at transforming the data collection process from a manual system to a digital platform. The mobile application has already been launched, and it is currently being developed into a website to enhance data management efficiency and accessibility.",
+    thumbnail_image: "/assets/villagefund/vf-1.jpg",
+  },
+  {
+    id: 2,
+    name: "Home Service By HomePro",
+    excerpt:
+      "Home Service by HomePro is a mobile application offering over 100 services, covering the widest service area in Thailand, including both Bangkok and provincial regions. Customers can access services anytime, anywhere, 24/7.",
+    thumbnail_image: "/assets/homeservice/homeservice-2.jpg",
+  },
+];
